567c573b58e68f1cef54a0971727ffe6d5fc1047
Jose M. Gomez <jgomepav@emeal.nttdata.com>
2022-04-08 12:54:29 +0200

    fix: Correcciones

--- src/app/modules/car/one/car-one.page.ts
+++ src/app/modules/car/one/car-one.page.ts
@@ -3,7 +3,6 @@ import { UserService } from 'src/app/services/user/user.service';
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { ActionForOptionI } from 'src/app/interfaces/action-for-option.interface';
-import { Inscription } from 'src/app/models/inscription.model';
 import { BrandService } from 'src/app/services/brand/brand.service';
 import { CarService } from 'src/app/services/car/car.service';
 import { InscriptionService } from 'src/app/services/inscription/inscription.service';
@@ -11,159 +10,159 @@ import { CarOnePageViewModel } from './model/car-one.view-model';
 import { WinnerService } from 'src/app/services/winner/winner.service';
 
 @Component({
-  selector: 'page-car-one',
-  templateUrl: 'car-one.page.html',
+    selector: 'page-car-one',
+    templateUrl: 'car-one.page.html',
 })
 export class CarOnePage implements OnInit {
-  vm = new CarOnePageViewModel();
-  constructor(
-    private route: ActivatedRoute,
-    private carService: CarService,
-    private brandService: BrandService,
-    private userService: UserService,
-    private voteService: VoteService,
-    private winnerService: WinnerService,
-    private inscriptionService: InscriptionService,
-    private router: Router
-  ) {}
+    vm = new CarOnePageViewModel();
+    constructor(
+        private route: ActivatedRoute,
+        private carService: CarService,
+        private brandService: BrandService,
+        private userService: UserService,
+        private voteService: VoteService,
+        private winnerService: WinnerService,
+        private inscriptionService: InscriptionService,
+        private router: Router
+    ) {}
 
-  ngOnInit() {
-    this.vm.id = this.route.snapshot.paramMap.get('id') as string;
-    this.getAllBrands();
-    this.getAllDrivers();
-    this.getAllWinners();
-    if (this.vm.id) {
-      this.vm.optionsTitle.title = 'Editar Coche';
-      this.vm.edit = true;
-      this.getInscriptionsByCar();
-      this.getVotesByCar();
-      this.getOne();
-    } else {
-      this.vm.optionsTitle.title = 'Nuevo Coche';
-      this.vm.edit = false;
+    ngOnInit() {
+        this.vm.id = this.route.snapshot.paramMap.get('id') as string;
+        this.getAllBrands();
+        this.getAllDrivers();
+        this.getAllWinners();
+        if (this.vm.id) {
+            this.vm.optionsTitle.title = 'Editar Coche';
+            this.vm.edit = true;
+            this.getInscriptionsByCar();
+            this.getVotesByCar();
+            this.getOne();
+        } else {
+            this.vm.optionsTitle.title = 'Nuevo Coche';
+            this.vm.edit = false;
+        }
     }
-  }
 
-  async getOne() {
-    this.carService.getOne(this.vm.id).subscribe({
-      next: (item) => {
-        this.vm.item = item;
-        this.vm.stock = this.vm.item.stock;
-        this.vm.brandIdSelected = item.brand._id;
-        this.vm.userIdSelected = item.driver._id;
-      },
-      error: (error) => console.error(error),
-    });
-  }
-
-  getAllDrivers() {
-    this.userService.getAll(this.vm.bodyUsers).subscribe({
-      next: (result) => (this.vm.users = result.items),
-      error: (e) => console.error(e),
-    });
-  }
-
-  getAllWinners() {
-    this.winnerService.getAll(this.vm.bodyWinners).subscribe({
-      next: (result) => (this.vm.winners = result.items),
-      error: (e) => console.error(e),
-    });
-  }
-
-  getAllBrands() {
-    this.brandService.getAll(this.vm.bodyBrands).subscribe({
-      next: (result) => (this.vm.brands = result.items),
-      error: (e) => console.error(e),
-    });
-  }
-
-  getInscriptionsByCar() {
-    this.inscriptionService.getAllOfCar({ id: this.vm.id }).subscribe({
-      next: (items) => {
-        this.vm.inscriptionsOptionsTable.loading = true;
-        this.vm.inscriptionsOptionsTable.items = items;
-        this.vm.inscriptionsOptionsTable.loading = false;
-      },
-      error: (e) => console.error(e),
-    });
-  }
-
-  getVotesByCar() {
-    this.voteService.getAllOfCar({ id: this.vm.id }).subscribe({
-      next: (items) => (this.vm.votesOptionsTable.items = items),
-      error: (e) => console.error(e),
-    });
-  }
-
-  async onSubmit() {
-    try {
-      this.vm.item.brand = this.vm.brandIdSelected;
-      this.vm.item.driver = this.vm.userIdSelected;
-      this.vm.item.stock = Boolean(this.vm.stock);
-      this.vm.edit
-        ? this.carService.update(this.vm.item).subscribe(() => {
-            this.router.navigate(['/cars']);
-          })
-        : this.carService.create(this.vm.item).subscribe(() => {
-            alert('Coche creado');
-            this.router.navigate(['/cars']);
-          });
-    } catch (error) {
-      console.error(error);
+    async getOne() {
+        this.carService.getOne(this.vm.id).subscribe({
+            next: (item) => {
+                this.vm.item = item;
+                this.vm.stock = this.vm.item.stock;
+                this.vm.brandIdSelected = item.brand._id;
+                this.vm.userIdSelected = item.driver._id;
+            },
+            error: (error) => console.error(error),
+        });
     }
-  }
 
-  actionForOption(option: ActionForOptionI) {
-    switch (option.value) {
-      case 'deleteInscriptions':
-        this.deleteInscriptions();
-        break;
-      case 'delete':
-        this.deleteOne();
-        break;
-      default:
-        break;
+    getAllDrivers() {
+        this.userService.getAll(this.vm.bodyUsers).subscribe({
+            next: (result) => (this.vm.users = result.items),
+            error: (e) => console.error(e),
+        });
     }
-  }
 
-  async deleteInscriptions() {
-    if (confirm('¿Estás seguro de eliminar todas las inscripciones?')) {
-      this.vm.inscriptionsOptionsTable.loading = true;
-      this.inscriptionService.deleteAllOfCar(this.vm.id).subscribe({
-        next: () => {
-          this.vm.inscriptionsOptionsTable.loading = false;
-          this.vm.inscriptionsOptionsTable.items = [];
-        },
-        error: (e) => {
-          this.vm.inscriptionsOptionsTable.loading = false;
-          console.error(e);
-        },
-      });
+    getAllWinners() {
+        this.winnerService.getAll(this.vm.bodyWinners).subscribe({
+            next: (result) => (this.vm.winners = result.items),
+            error: (e) => console.error(e),
+        });
     }
-  }
 
-  async deleteOne() {
-    if (confirm('¿Está seguro de eliminar el coche?')) {
-      this.carService.delete(this.vm.id).subscribe({
-        next: () => {
-          alert('Coche eliminado');
-          this.router.navigate(['/cars']);
-        },
-        error: (e) => alert(e),
-      });
+    getAllBrands() {
+        this.brandService.getAll(this.vm.bodyBrands).subscribe({
+            next: (result) => (this.vm.brands = result.items),
+            error: (e) => console.error(e),
+        });
     }
-  }
 
-  onInscription() {
-    this.getInscriptionsByCar();
-  }
-
-  onDeleteInscription(id: string) {
-    if (confirm('¿Está seguro de eliminar la inscripcion?')) {
-      this.inscriptionService.deleteOne(id).subscribe({
-        next: () => this.getInscriptionsByCar(),
-        error: (error) => console.error(error),
-      });
+    getInscriptionsByCar() {
+        this.inscriptionService.getAllOfCar({ id: this.vm.id }).subscribe({
+            next: (items) => {
+                this.vm.inscriptionsOptionsTable.loading = true;
+                this.vm.inscriptionsOptionsTable.items = items;
+                this.vm.inscriptionsOptionsTable.loading = false;
+            },
+            error: (e) => console.error(e),
+        });
     }
-  }
+
+    getVotesByCar() {
+        this.voteService.getAllOfCar({ id: this.vm.id }).subscribe({
+            next: (items) => (this.vm.votesOptionsTable.items = items),
+            error: (e) => console.error(e),
+        });
+    }
+
+    async onSubmit() {
+        try {
+            this.vm.item.brand = this.vm.brandIdSelected;
+            this.vm.item.driver = this.vm.userIdSelected;
+            this.vm.item.stock = Boolean(this.vm.stock);
+            this.vm.edit
+                ? this.carService.update(this.vm.item).subscribe(() => {
+                      this.router.navigate(['/cars']);
+                  })
+                : this.carService.create(this.vm.item).subscribe(() => {
+                      alert('Coche creado');
+                      this.router.navigate(['/cars']);
+                  });
+        } catch (error) {
+            console.error(error);
+        }
+    }
+
+    actionForOption(option: ActionForOptionI) {
+        switch (option.value) {
+            case 'deleteInscriptions':
+                this.deleteInscriptions();
+                break;
+            case 'delete':
+                this.deleteOne();
+                break;
+            default:
+                break;
+        }
+    }
+
+    async deleteInscriptions() {
+        if (confirm('¿Estás seguro de eliminar todas las inscripciones?')) {
+            this.vm.inscriptionsOptionsTable.loading = true;
+            this.inscriptionService.deleteAllOfCar(this.vm.id).subscribe({
+                next: () => {
+                    this.vm.inscriptionsOptionsTable.loading = false;
+                    this.vm.inscriptionsOptionsTable.items = [];
+                },
+                error: (e) => {
+                    this.vm.inscriptionsOptionsTable.loading = false;
+                    console.error(e);
+                },
+            });
+        }
+    }
+
+    async deleteOne() {
+        if (confirm('¿Está seguro de eliminar el coche?')) {
+            this.carService.delete(this.vm.id).subscribe({
+                next: () => {
+                    alert('Coche eliminado');
+                    this.router.navigate(['/cars']);
+                },
+                error: (e) => alert(e),
+            });
+        }
+    }
+
+    onInscription() {
+        this.getInscriptionsByCar();
+    }
+
+    onDeleteInscription(id: string) {
+        if (confirm('¿Está seguro de eliminar la inscripcion?')) {
+            this.inscriptionService.deleteOne(id).subscribe({
+                next: () => this.getInscriptionsByCar(),
+                error: (error) => console.error(error),
+            });
+        }
+    }
 }
