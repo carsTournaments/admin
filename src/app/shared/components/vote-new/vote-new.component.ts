147f4c1ce02c642aea17e8e358debc11e672e9a3
JoseXS <XSkunk@gmail.com>
2022-05-26 13:43:06 +0200

    fix: Correcciones

--- src/app/shared/components/vote-new/vote-new.component.ts
+++ src/app/shared/components/vote-new/vote-new.component.ts
@@ -1,6 +1,6 @@
 import { Component, Inject, OnInit } from '@angular/core';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
-import { Car, Pairing, Round, Tournament } from '@models';
+import { Car } from '@models';
 import { SnackBarService, VoteService } from '@services';
 import { VoteCreateDto } from '@services/api/vote/dtos/vote.dto';
 
