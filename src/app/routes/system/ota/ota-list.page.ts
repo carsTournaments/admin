2ed8b274db1fa3468d5cd2d1aaa236fe7247e865
JoseXS <XSkunk@gmail.com>
2022-06-01 23:13:25 +0200

    fix: Correcciones

--- src/app/routes/system/ota/ota-list.page.ts
+++ src/app/routes/system/ota/ota-list.page.ts
@@ -1,11 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
     selector: 'ota-list',
     templateUrl: 'ota-list.page.html',
 })
-export class OtaListPage implements OnInit {
-    constructor() {}
-
-    ngOnInit() {}
-}
+export class OtaListPage {}
